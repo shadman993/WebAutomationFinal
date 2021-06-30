719f954eae879e1428430dae59fb18222e8955e4
shadman <shadman@shadmans-MBP.fios-router.home>
2021-06-30 19:41:44 -0400

    initial commit

---
+++ Amazon/src/test/java/com/amazon/tests/addtocarttest/ChangeQuantityFromAddToCartTest.java
@@ -0,0 +1,29 @@
+package com.amazon.tests.addtocarttest;
+
+
+import addtocartpage.ChangeQuantityFromAddToCart;
+//import com.amazon.addtocartpage.ChangeQuantityFromAddToCart;
+import common.TestBase;
+import org.apache.log4j.Logger;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.Test;
+
+public class ChangeQuantityFromAddToCartTest extends TestBase {
+    private static final Logger logger = Logger.getLogger(ChangeQuantityFromAddToCartTest.class);
+     private ChangeQuantityFromAddToCart changeQuantityFromAddToCart;
+
+    @Test
+    public void validateChangeQuantityTest(){
+        changeQuantityFromAddToCart = PageFactory.initElements(driver,ChangeQuantityFromAddToCart.class);
+        changeQuantityFromAddToCart.ClickOnCartButton();
+        changeQuantityFromAddToCart.clickOnQuantityButton();
+        changeQuantityFromAddToCart.clickOnQuantityNumber();
+
+
+    }
+
+
+
+
+
+}
