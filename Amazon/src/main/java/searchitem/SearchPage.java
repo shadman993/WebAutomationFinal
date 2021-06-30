0467d02851e3235796e1fc3d714b228b2943549f
shadman <shadman@shadmans-MBP.fios-router.home>
2021-06-30 19:22:00 -0400

    initial commit

---
+++ Amazon/src/main/java/searchitem/SearchPage.java
@@ -0,0 +1,28 @@
+package searchitem;
+
+import common.TestBase;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class SearchPage extends TestBase {
+
+    @FindBy(id = "twotabsearchtextbox")
+    private WebElement searchTextBox;
+
+    @FindBy(id = "nav-search-submit-button")
+    private WebElement searchButton ;
+
+    public void typeOnSearchBar(String text){
+      searchTextBox.sendKeys(text);
+    }
+
+    public void clickOnSearchButton(){
+        searchButton.click();
+    }
+
+    public String searchPageTitle(){
+      return  getTitle();
+    }
+
+
+}
